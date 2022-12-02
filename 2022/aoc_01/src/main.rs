5bf7d82baa0be57ebae87c3f45178ded1e71cf18
rihavit <vit.riha.97@gmail.com>
2022-12-02 17:04:25 +0100

    [2022-01] Add Part 2 solution

--- 2022/aoc_01/src/main.rs
+++ 2022/aoc_01/src/main.rs
@@ -8,11 +8,23 @@ fn main() {
 
     let input = fs::read_to_string(input_file_path).expect("Should have been able to read the file.");
     let elves: Vec<&str> = input.split("\n\r\n").collect();
-    let mut result: u32 = 0;
+    let mut top_three: Vec<u32> = Vec::new();
+    top_three.reserve(3);
     for elf in elves {
-        result = max(elf.split("\n")
-                         .map(|s| if s.is_empty() {0} else {s[..s.len()-1].parse::<u32>().unwrap_or(0)})
-                         .sum(), result);
+        let total_elf = elf.split("\n")
+            .map(|s| if s.is_empty() {0} else {s[..s.len()-1].parse::<u32>().unwrap_or(0)})
+            .sum();
+        if top_three.len() < 3 {
+            top_three.push(total_elf);
+            continue;
+        }
+        for i in 0..3 {
+            if top_three[i] < total_elf {
+                top_three[i] = total_elf;
+                break;
+            }
+        }
     }
-    println!("{result}");
+    println!("Max elf: {}", max(max(top_three[0], top_three[1]), top_three[2]));
+    println!("Total elves: {}", top_three.iter().sum::<u32>());
 }
