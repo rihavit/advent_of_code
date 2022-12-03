91c3985a467e8c665b9997a64c664eee6efbdbd0
Vit Riha <vit.riha.97@gmail.com>
2022-12-03 16:15:07 +0100

    [2022-03] Solve Part 1

--- 2022/aoc_03/src/main.rs
+++ 2022/aoc_03/src/main.rs
@@ -2,5 +2,23 @@ mod input_reader;
 
 fn main() {
     let input = input_reader::read_file();
-    println!("Hello, world!");
+    let result: u16 = input.split('\n').map(|rucksack| {
+        let items = rucksack.chars().collect::<Vec<char>>();
+        let cmp_a = &items[..items.len()/2];
+        let cmp_b = &items[items.len()/2..];
+        return match cmp_a.iter().find(|item| cmp_b.contains(item)) {
+            Some(x) => item_to_priority(x),
+            _ => panic!("No common item found!")
+        }
+    }).sum();
+    println!("{result}");
+}
+
+fn item_to_priority(item: &char) -> u16 {
+    let ascii_code = *item as u16;
+    return if ascii_code > 90 {
+        ascii_code - 96
+    } else {
+        ascii_code - 38
+    }
 }
