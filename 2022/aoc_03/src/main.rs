2340050351eb2b25e12d82c2a8c920b8231501b8
Vit Riha <vit.riha.97@gmail.com>
2022-12-03 22:20:25 +0100

    [2022-03] Solve Part 2

--- 2022/aoc_03/src/main.rs
+++ 2022/aoc_03/src/main.rs
@@ -2,7 +2,7 @@ mod input_reader;
 
 fn main() {
     let input = input_reader::read_file();
-    let result: u16 = input.split('\n').map(|rucksack| {
+    let sum_of_priorities: u16 = input.split('\n').map(|rucksack| {
         let items = rucksack.chars().collect::<Vec<char>>();
         let cmp_a = &items[..items.len()/2];
         let cmp_b = &items[items.len()/2..];
@@ -11,7 +11,21 @@ fn main() {
             _ => panic!("No common item found!")
         }
     }).sum();
-    println!("{result}");
+    println!("Sum of common items' priorities: {sum_of_priorities}");
+
+    let sum_of_badges: u16 = input.split('\n').collect::<Vec<&str>>().chunks(3).map(|group| -> u16 {
+        let rucksacks = group.iter().map(|elf| elf.chars().collect()).collect::<Vec<Vec<char>>>();
+        return match &rucksacks[..] {
+            [ rucksack_a, rucksack_b, rucksack_c ] => {
+                let badge = rucksack_a.iter().find(|item| {
+                    rucksack_b.contains(item) && rucksack_c.contains(item)
+                }).unwrap();
+                item_to_priority(badge)
+            },
+            _ => panic!("Group did not contain three rucksacks.")
+        }
+    }).sum();
+    println!("Sum of badges' priorities: {sum_of_badges}")
 }
 
 fn item_to_priority(item: &char) -> u16 {
